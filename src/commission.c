59c245dd3b7307c785d814ddf0ce8a7171c0dfa1
Vicente Marçal <vicente.marcal@gmail.com>
2025-09-17 20:55:27 -0300

    Add program to calculate total salary of a salesman based on sales and commission

---
+++ src/commission.c
@@ -0,0 +1,38 @@
+/**
+  * This program prompts the user to enter the salesman's code, total sales made,
+ * unit price of the product, fixed salary, and commission rate (as a percentage).
+ * It then calculates the total amount of sales, computes the commission based on
+ * the given rate, and sums it with the fixed salary to determine the total salary.
+ * The result is displayed with two decimal places.
+ *
+ * Input:
+ *   - Salesman's code (integer)
+ *   - Total sales made (integer)
+ *   - Unit price (double)
+ *   - Fixed salary (double)
+ *   - Commission rate in percentage (double)
+ *
+ * Output:
+ *   - Total salary of the salesman, formatted as currency.
+ */
+#include <stdio.h>
+
+int main(){
+    int sales_man_code, total_sales;
+    double total_salary, total_commission, salary, commission_rate, amount_sales, unit_price;
+    printf("Enter the salesman's code: ");
+    scanf("%d", &sales_man_code);
+    printf("Enter the total sales made: ");
+    scanf("%d", &total_sales);
+    printf("Enter the unit price: ");
+    scanf("%lf", &unit_price);
+    amount_sales = total_sales * unit_price;
+    printf("Enter the fixed salary: ");
+    scanf("%lf", &salary);
+    printf("Enter the commission rate (in percentage): ");
+    scanf("%lf", &commission_rate);
+    total_commission = (commission_rate / 100) * amount_sales;
+    total_salary = salary + total_commission;
+    printf("The total salary of salesman %d is: R$%.2lf\n", sales_man_code, total_salary);
+    return 0;
+}
