38e400ae4afa74799406beedb54756d9e39f5e9e
Vicente Marçal <vicente.marcal@gmail.com>
2025-09-24 21:00:47 -0300

    Refactor FizzBuzz program to accept integer input and print results for numbers 1 to 100

--- src/fizz_buzz.c
+++ src/fizz_buzz.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int number;
-
-    printf("Enter an integer: ");
-    scanf("%d",  &number);
-    if (number % 3 == 0 && number % 5 == 0) {
+int fizz_buzz(int num) {
+    if (num % 3 == 0 && num % 5 == 0) {
         printf("FizzBuzz\n");
-    } else if (number % 3 == 0) {
+    } else if (num % 3 == 0) {
         printf("Fizz\n");
-    } else if (number % 5 == 0) {
+    } else if (num % 5 == 0) {
         printf("Buzz\n");
     } else {
-        printf("%d\n", number);
+        printf("%d\n", num);
+    }
+    return 0;
+}
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+        fizz_buzz(i);
     }
     return 0;
 }
